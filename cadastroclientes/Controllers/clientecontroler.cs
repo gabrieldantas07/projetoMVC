d7d2c6f39d738238470172869c808b2c75eca207
Gabriel Dantas <gabrieldantas50@etec.sp.gov.br>
2023-07-25 11:11:34 -0300

    Primeira Versão do Projeto

---
+++ cadastroclientes/Controllers/clientecontroler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cadastroclientes.Controllers
+{
+    public class clientecontroler : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ cadastroclientes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cadastroclientes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
